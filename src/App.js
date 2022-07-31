a6d19df7bcf8ffeda2d353c2714b834588ab312f
Ashok <mettaa.games@gmail.com>
2022-07-31 21:35:18 +0800

    loaded the drawing model

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useRef, useEffect, useState } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as posenet from "@tensorflow-models/posenet";
 import Webcam from "react-webcam";
-
+import { keypoints, drawSkeleton, drawKeypoints } from "./utils";
 function App() {
   const [windowSize, setWindowSize] = useState(getWindowSize());
 
@@ -18,23 +18,76 @@ function App() {
       window.removeEventListener("resize", handleWindowResize);
     };
   }, []);
+
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  //  Load posenet
+  const runPosenet = async () => {
+    const net = await posenet.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 0.8,
+    });
+    //
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Make Detections
+      const pose = await net.estimateSinglePose(video);
+      //console.log(pose);
+
+      drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+    }
+  };
+
+  const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
+    const ctx = canvas.current.getContext("2d");
+    canvas.current.width = videoWidth;
+    canvas.current.height = videoHeight;
+
+    drawKeypoints(pose["keypoints"], 0.6, ctx);
+    drawSkeleton(pose["keypoints"], 0.7, ctx);
+  };
+
+  runPosenet();
+
   return (
     <div className="App">
       <header className="App-header">
         <Webcam
+          ref={webcamRef}
           style={{
             position: "absolute",
             marginLeft: "auto",
-            left: "0",
-            right: "0",
-            marginTop: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
             textAlign: "center",
-            zIndex: 9,
-            width: window.innerWidth,
-            height: window.innerHeight,
+            zindex: 9,
+            width: 640,
+            height: 480,
+            visibility: "hidden",
           }}
         />
         <canvas
+          ref={canvasRef}
           style={{
             position: "absolute",
             marginLeft: "auto",
@@ -43,8 +96,9 @@ function App() {
             right: "0",
             textAlign: "center",
             zIndex: 9,
-            width: window.innerWidth,
-            height: window.innerHeight,
+            width: 640 / 2,
+            height: 480 / 2,
+            backgroundColor: "rgba(255,0,0,0.5)",
           }}
         />
       </header>
